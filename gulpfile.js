d69914695761c7046da08035336e3eda7bb5a0f9
thatjs <dev@thatjs.com>
2015-09-27 11:18:31 -0700

    changed config file to a template, and check that it exists, gitignore working as expected

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,18 @@ var startTime = +new Date(),
     argv = require('minimist')(process.argv.slice(2)),
 
     jslint = require('gulp-jslint'),
+    rename = require('gulp-rename'),
     requireDir = require('require-dir'),
     shell = require('gulp-shell'),
 
     gulpSync = require('gulp-sync')(gulp),
     os = require('os'),
-    debug = require('gulp-debug');
+    debug = require('gulp-debug'),
+    fs = require('fs'),
+
+    path = {
+        config: './config'
+    };
 
 var srcGulp = 'gulpfile.js',
     srcSvr = 'appServer/*.js',
@@ -31,6 +37,17 @@ gulp.task('info', function () {
         ]));
 });
 
+gulp.task('init', function () {
+    // check for config/jenkins.json, create if not found
+    if (!fs.existsSync(path.config + '/jenkins.json')) {
+        return gulp.src(path.config + '/templates/jenkins.tmpl.json')
+            .pipe(rename('/jenkins.json'))
+            .pipe(gulp.dest(path.config));
+    } else {
+        return gUtil.noop(); // return the stream
+    }
+});
+
 // include as the last task to display the total build time
 gulp.task('totalTime', function () {
     var totalTime = new Date(+new Date() - startTime);
@@ -42,7 +59,9 @@ gulp.task('totalTime', function () {
 // ===============
 
 // use config/jenkins.json
-gulp.task('jenkinsProxy', function () {
+gulp.task('jenkinsProxy', ['init'], function () {
+
+
     var proxy = require('./appServer').jenkinsProxy;
     proxy({
         port: 8086
@@ -50,7 +69,7 @@ gulp.task('jenkinsProxy', function () {
 });
 
 // override config/jenkins.json
-gulp.task('jenkinsProxy2', function () {
+gulp.task('jenkinsProxy2', ['init'], function () {
     var proxy = require('./appServer').jenkinsProxy;
     proxy({
         port: 8086,
